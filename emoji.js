f9da2b6b5e2cc10c7e7f96b6055d154ee75bd08c
Artjoms Avramuks <artjomsavramuks@gmail.com>
2019-05-14 14:31:01 +0300

    Emojidarbs

---
+++ emoji.js
@@ -0,0 +1,43 @@
+function atzime (atz){
+    var atzime = console.readLine();
+    if(atzime == 1)
+    {
+      console.log(atzime+" 😓 ")
+    }
+    else if(atzime == 2)
+    {
+      console.log(atzime+"☹️")
+    }
+    else if(atzime == 3)
+    {
+      console.log(atzime+"🙁")
+    }
+    else if(atzime == 4)
+    {
+      console.log(atzime+"😰")
+    }
+    else if(atzime == 5)
+    {
+      console.log(atzime+"😐")
+    }
+    else if(atzime == 6)
+    {
+      console.log(atzime+"🙃")
+    }
+    else if(atzime == 7)
+    {
+      console.log(atzime+"😊")
+    }
+    else if(atzime == 8)
+    {
+      console.log(atzime+"🤯")
+    }
+    else if(atzime == 9)
+    {
+      console.log(atzime+"🤑")
+    }
+    else if(atzime == 10)
+    {
+      console.log(atzime+"🤗")
+    }
+    }
